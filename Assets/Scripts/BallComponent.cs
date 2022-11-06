bd6c97b5637a6cfdd3c3ff9f7ce7d338c668c954
Zajceq <l.gebicz@gmail.com>
2022-11-06 01:08:54 +0100

    Added ball transform changes

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class BallComponent : MonoBehaviour
 {
+    public float Speed = 1.0f;
+    public float RotationSpeed = 10.0f;
+    private Vector3 vecRotation = Vector3.zero;
+
     void Update()
     {
-         Debug.Log("Frames per second: " + 1/Time.deltaTime);
+        //Debug.Log("Frames per second: " + 1/Time.deltaTime);
+        transform.position += Vector3.up * Time.deltaTime * Speed;
+        vecRotation += Vector3.forward * RotationSpeed;
+        transform.rotation = Quaternion.Euler(vecRotation);
+        if (transform.localScale.x < 3)
+        {
+             transform.localScale += Vector3.one * Time.deltaTime * Speed;
+        }
+        else
+        {
+            transform.localScale = new Vector3(3, 3, 3);
+        }
     }
 }
