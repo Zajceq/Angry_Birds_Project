a1931410abcc228bd40e8dc5d92e2830905859e5
Zajceq <l.gebicz@gmail.com>
2023-01-27 09:56:04 +0100

    Added new test function

--- Assets/Tests/PlayMode/SpawnerTest.cs
+++ Assets/Tests/PlayMode/SpawnerTest.cs
@@ -2,69 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 public class SpawnerTest
 {
-    //// A Test behaves as an ordinary method
-    //[Test]
-    //public void SpawnerTestSimplePasses()
-    //{
-    //    // Use the Assert class to test conditions
-    //}
-
-    //// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    //// `yield return null;` to skip a frame.
-    //[UnityTest]
-    //public IEnumerator SpawnerTestWithEnumeratorPasses()
-    //{
-    //    // Use the Assert class to test conditions.
-    //    // Use yield to skip a frame.
-    //    var gameObject = new GameObject();
-    //    var prefabSpawner = gameObject.AddComponent<TestDrivenPrefabSpawner>();
-    //    prefabSpawner.SpawnPrefabs();
-    //    var spawnedGameObjects = GameObject.FindGameObjectsWithTag(prefabSpawner.PrefabTag);
-    //    Assert.AreEqual(3, spawnedGameObjects.Length);
-    //    yield return null;
-
-    //}
-    //[Test]
-    //public void TestSceneHasThreeObjectsWithSameTag()
-    //{
-    //    var objectsWithTag = GameObject.FindGameObjectsWithTag("TargetFull");
-    //    Assert.AreEqual(3, objectsWithTag.Length, "There should be exactly 3 objects with tag 'TargetFull' on the scene.");
-    //}
-
-    //[UnityTest]
-    //public IEnumerator CheckTagObjects()
-    //{
-    //    var gameObject = new GameObject();
-    //    var prefabSpawner = gameObject.AddComponent<TestDrivenPrefabSpawner>();
-
-    //    GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag(prefabSpawner.PrefabTag);
-
-    //    int expectedAmount = 3;
-    //    int actualAmount = objectsWithTag.Length;
-
-    //    Assert.AreEqual(expectedAmount, actualAmount);
-
-    //    yield return null;
-    //}
+    [OneTimeSetUp]
+    public void Init()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
 
     [UnityTest]
-    public IEnumerator TestSpawnPrefabs()
+    public IEnumerator CheckTagObjects()
     {
-        // Find the TestDrivenPrefabSpawner script
-        var spawner = GameObject.FindObjectOfType<TestDrivenPrefabSpawner>();
+        var prefabSpawner = GameObject.FindObjectOfType<TestDrivenPrefabSpawner>();
 
-        // Call the SpawnPrefabs function
-        spawner.SpawnPrefabs();
+        GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag(prefabSpawner.PrefabTag);
 
-        // Wait for one frame
+        int expectedAmount = 3;
+        int actualAmount = objectsWithTag.Length;
+
+        Assert.AreEqual(expectedAmount, actualAmount);
+
         yield return null;
-
-        // Check that there are exactly 3 objects with the same tag on the scene
-        var objectsWithTag = GameObject.FindGameObjectsWithTag("TargetFull");
-        Assert.AreEqual(3, objectsWithTag.Length, "There should be exactly 3 objects with tag 'SameTag' on the scene.");
     }
+
 }
